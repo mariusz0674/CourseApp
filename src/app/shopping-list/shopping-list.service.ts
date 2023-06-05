68a3cee3de514b35a49496323d463761d82e2d9d
mariusz0674 <83462478+mariusz0674@users.noreply.github.com>
2023-06-05 21:49:39 +0200

    Commit

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,28 @@
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from "../shared/ingredient.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 69),
+  ];
+  constructor() { }
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients); // ... is the spread operator
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+}
